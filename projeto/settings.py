ef3f7df447d21656e354e5c5240b04618ed5a38d
Kalebe16 <kalebecalmeida@hotmail.com>
2022-10-22 23:00:32 -0300

    d

--- projeto/settings.py
+++ projeto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*h75l2afd$c$6s@dl$ype-g0c99f+=ni%&ap=v3ga7n8b5nzp5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
